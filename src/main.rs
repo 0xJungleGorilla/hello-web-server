48adbb4a11abd763917edebdc152325fcb740e59
Gorilla 0xJungle.com <Gorilla@0xJungle.com>
2023-01-23 00:25:51 -0500

    added multithreading support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use hello_web_server::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -8,10 +9,14 @@ use std::{
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established!");
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
